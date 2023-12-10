d05711c9fd0e76ef299fd991f9c64a2930820ae7
Chris <741258+github@pm.me>
2023-12-10 07:51:12 -0600

    add rule and separate css

--- moneyterm/screens/tagselectorscreen.py
+++ moneyterm/screens/tagselectorscreen.py
@@ -20,20 +20,22 @@ from textual.widgets import (
     Input,
     Markdown,
 )
-from textual.containers import Vertical, Horizontal, VerticalScroll
+from textual.containers import Vertical, Horizontal, VerticalScroll, Middle
 from moneyterm.utils.ledger import Ledger, Transaction
 from moneyterm.utils.financedb import FinanceDB
 from pathlib import Path
 
 
-class TagSelectorScreen(Screen):
+class TransactionTaggerScreen(ModalScreen):
     """Screen for selecting a tag from a list of all tags in the ledger. Tags are filtered by the input field.
 
     Args:
         Screen (textual.screen.Screen): Screen class
     """
 
-    def __init__(self, ledger: Ledger) -> None:
+    CSS_PATH = "../tcss/tagselectorscreen.tcss"
+
+    def __init__(self, ledger: Ledger, transaction: Transaction) -> None:
         """Initialize the screen.
 
         Args:
@@ -41,18 +43,23 @@ class TagSelectorScreen(Screen):
         """
         super().__init__()
         self.ledger = ledger
+        self.transaction = transaction
+        self.vertical_scroll = VerticalScroll()
+        self.vertical_scroll.can_focus = False
+        self.vertical_scroll.border_title = "Quick Tag"
 
     def compose(self) -> ComposeResult:
         self.tag_list: OptionList = OptionList(id="tag_list")
-
-        with Vertical():
-            yield Input(placeholder="Search Tags")
-            yield Label("Select Tag")
+        with self.vertical_scroll:
+            yield Input(placeholder="Search Tags", id="tag_search_input")
+            yield Rule()
+            yield Label("Available Tags")
             yield self.tag_list
 
     def on_mount(self) -> None:
         self.all_tags = self.ledger.get_all_tags()
         self.tag_list.add_options(self.all_tags)
+        self.query_one("#tag_search_input").focus()
 
     def on_key(self, key: events.Key) -> None:
         if key.key == "escape":
