c9263055ec4e7b2a27d4e29df015bbc4104c9054
Chris <741258+github@pm.me>
2023-12-27 06:23:23 -0600

    adds sorting and labels

--- moneyterm/widgets/transactiontable.py
+++ moneyterm/widgets/transactiontable.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from decimal import Decimal
 from textual import events
 from textual.reactive import reactive
 from textual.types import NoSelection
@@ -28,11 +30,19 @@ class TransactionTable(DataTable):
         self.cursor_type = "row"
         self.id = "transactions_table"
         self.selected_row_key: str | None = None
+        self.column_labels = ["Date", "Payee", "Type", "Amount", "Account", "Labels"]
+        self.last_sort_label: str = ""
+        self.reversed_sort: bool = False
+
+    def add_columns_from_labels(self) -> None:
+        """Add columns from column keys."""
+        for label in self.column_labels:
+            self.add_column(label, key=label)
 
     def update_data(self) -> None:
         """Update the datatable when month selection changed."""
         self.clear(columns=True)
-        self.add_columns("Date", "Payee", "Type", "Amount", "Account", "Categories")
+        self.add_columns_from_labels()
         if (
             isinstance(self.account, NoSelection)
             or isinstance(self.year, NoSelection)
@@ -45,46 +55,38 @@ class TransactionTable(DataTable):
         for tx in self.ledger.get_tx_by_month(self.account, self.year, self.month):
             self.add_transaction_row(tx)
 
-    def add_transaction_row(self, tx: Transaction, payee_alias: str = "", account_alias: str = "") -> None:
+    def add_transaction_row(self, tx: Transaction) -> None:
         self.cursor_type = "row"
+        labels = ",".join(sorted(tx.labels.bills + tx.labels.categories + tx.labels.incomes))
         self.add_row(
             tx.date.strftime("%Y-%m-%d"),
-            payee_alias if payee_alias else tx.payee,
+            tx.alias if tx.alias else tx.payee,
             tx.tx_type,
             tx.amount,
-            account_alias if account_alias else tx.account_number,
-            ",".join(tx.categories),
+            tx.account.alias if tx.account.alias else tx.account.number,
+            labels,
             key=tx.txid,
         )
 
     def on_mount(self) -> None:
         """Mount the datatable."""
-        self.add_columns("Date", "Payee", "Type", "Amount", "Account", "Categories")
+        self.add_columns_from_labels()
 
     def on_key(self, key: events.Key) -> None:
-        def update_row_categories(*args):
-            if self.selected_row_key is None:
-                return
-            self.update_cell(
-                self.selected_row_key,
-                "CATEGORIES",
-                ",".join(sorted(self.ledger.get_tx_by_txid(self.selected_row_key).categories)),
-            )
+        # def get_selected_category(category: str):
+        #     if category:
+        #         self.apply_category_to_selected_transaction(category)
+        #         update_row_categories()
 
-        def get_selected_category(category: str):
-            if category:
-                self.apply_category_to_selected_transaction(category)
-                update_row_categories()
-
-        if key.key == "c":
-            if self.selected_row_key:
-                selected_transaction = self.ledger.get_tx_by_txid(self.selected_row_key)
-                self.app.push_screen(QuickCategoryScreen(self.ledger, selected_transaction), get_selected_category)
-        elif key.key == "i":
+        # if key.key == "t":
+        #     if self.selected_row_key:
+        #         selected_transaction = self.ledger.get_tx_by_txid(self.selected_row_key)
+        #         self.app.push_screen(QuickCategoryScreen(self.ledger, selected_transaction), get_selected_category)
+        if key.key == "i":
             if self.selected_row_key:
                 self.log(f"Showing transaction details for {self.selected_row_key}")
                 transaction = self.ledger.get_tx_by_txid(self.selected_row_key)
-                self.app.push_screen(TransactionDetailScreen(self.ledger, transaction), update_row_categories)
+                self.app.push_screen(TransactionDetailScreen(self.ledger, transaction))
 
         elif key.key == "I":
             if self.selected_row_key:
@@ -96,13 +98,39 @@ class TransactionTable(DataTable):
     def on_data_table_row_highlighted(self, event: DataTable.RowHighlighted) -> None:
         self.selected_row_key = event.row_key.value
 
-    def apply_category_to_selected_transaction(self, category: str) -> None:
-        """Apply a category to the selected transaction."""
-        if self.selected_row_key is None:
-            return
-        self.log(f"Applying category {category} to transaction {self.ledger.get_tx_by_txid(self.selected_row_key)}")
-        selected_transaction = self.ledger.get_tx_by_txid(self.selected_row_key)
-        self.ledger.add_category_to_tx(selected_transaction, category)
+    def on_data_table_header_selected(self, event: DataTable.HeaderSelected) -> None:
+        """
+        Sorts the transaction table based on the selected header label.
+
+        Args:
+            event (DataTable.HeaderSelected): The event object containing the selected header label.
+
+        Returns:
+            None
+        """
+        if str(event.label) == self.last_sort_label:
+            self.reversed_sort = not self.reversed_sort
+        else:
+            self.reversed_sort = False
+        self.last_sort_label = str(event.label)
+        if str(event.label) == "Labels":
+            self.log("Sorting by labels")
+            self.sort("Labels", key=lambda label: label.lower(), reverse=self.reversed_sort)
+        elif str(event.label) == "Date":
+            self.log("Sorting by date")
+            self.sort("Date", key=lambda date: datetime.strptime(date, "%Y-%m-%d"), reverse=self.reversed_sort)
+        elif str(event.label) == "Payee":
+            self.log("Sorting by payee")
+            self.sort("Payee", key=lambda payee: payee.lower(), reverse=self.reversed_sort)
+        elif str(event.label) == "Type":
+            self.log("Sorting by type")
+            self.sort("Type", key=lambda tx_type: tx_type.lower(), reverse=self.reversed_sort)
+        elif str(event.label) == "Amount":
+            self.log("Sorting by amount")
+            self.sort("Amount", key=lambda amount: Decimal(amount), reverse=self.reversed_sort)
+        elif str(event.label) == "Account":
+            self.log("Sorting by account")
+            self.sort("Account", reverse=self.reversed_sort)
 
     def watch_account(self) -> None:
         """Watch for account selection changes."""
