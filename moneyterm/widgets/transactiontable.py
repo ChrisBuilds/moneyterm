eaf9a3ac324831e1c5697b09febbf1b7d1844f49
Chris <741258+github@pm.me>
2024-01-02 10:13:02 -0600

    Add account aliases and load config from JSON file

--- moneyterm/widgets/transactiontable.py
+++ moneyterm/widgets/transactiontable.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from pathlib import Path
+import json
 from decimal import Decimal
 from textual import events
 from textual.reactive import reactive
@@ -28,6 +30,7 @@ class TransactionTable(DataTable):
         self.ledger = ledger
         self.zebra_stripes = True
         self.cursor_type = "row"
+        self.account_aliases: dict[str, str] = {}
         self.id = "transactions_table"
         self.selected_row_key: str | None = None
         self.column_labels = ["Date", "Payee", "Type", "Amount", "Account", "Labels"]
@@ -41,6 +44,7 @@ class TransactionTable(DataTable):
 
     def update_data(self) -> None:
         """Update the datatable when month selection changed."""
+        self.load_config_json()
         self.clear(columns=True)
         self.add_columns_from_labels()
         if (
@@ -58,15 +62,28 @@ class TransactionTable(DataTable):
     def add_transaction_row(self, tx: Transaction) -> None:
         self.cursor_type = "row"
         labels = ",".join(sorted(tx.labels.bills + tx.labels.categories + tx.labels.incomes))
+        account_alias = self.account_aliases.get(tx.account.number, tx.account)
         self.add_row(
             tx.date.strftime("%Y-%m-%d"),
             tx.alias if tx.alias else tx.payee,
             tx.tx_type,
             tx.amount,
-            tx.account.alias if tx.account.alias else tx.account.number,
+            account_alias,
             labels,
             key=tx.txid,
         )
+
+    def load_config_json(self) -> None:
+        try:
+            with open(Path("moneyterm/data/config.json"), "r") as f:
+                config = json.load(f)
+        except FileNotFoundError as e:
+            config = {"import_directory": "", "import_extension": "", "account_aliases": {}}
+        except json.decoder.JSONDecodeError as e:
+            config = {"import_directory": "", "import_extension": "", "account_aliases": {}}
+        if "account_aliases" in config and isinstance(config["account_aliases"], dict):
+            for account, alias in config["account_aliases"].items():
+                self.account_aliases[account] = alias
 
     def on_mount(self) -> None:
         """Mount the datatable."""
